517962d819efc8f53a4c1187b49ff9dfad3d2ed9
filippettijuanm <filippetti_juanm@hotmail.com>
2022-12-26 19:53:55 -0300

    correccion 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hola")
+console.log("Bienvenido a su tienda virtual")
+
+let carrito = Number(prompt("Ingrese el numero del juguete que desea adquirir: \n 1-Avion \n 2-Camion \n 3-Autito 4-Tractor \n 5-Muñecos \ 0-Para salir e ir al carrito de compras"))
+
+while (carrito != 0) {
+    let carrito = Number(prompt("Ingrese el numero del juguete que desea adquirir: \n 1-Avion \n 2-Camion \n 3-Autito 4-Tractor \n 5-Muñecos \n 0-Para salir e ir al carrito de compras"))
+
+}
+
+
+
+
+
